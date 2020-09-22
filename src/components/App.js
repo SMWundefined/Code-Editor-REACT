e3e44ca22db793a0546e0c53123732c7a3c9fad4
Wadood Sultan <wadood@Wadoods-MacBook-Pro.local>
2020-09-21 21:31:57 -0700

    Editor and CSS Structure done

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Editor from './Editor'
 
 function App() {
-  return "Hello World"
+  const [html, setHTML] = useState('')
+  const [css, setCSS] = useState('')
+  const [js, setJS] = useState('')
+
+  return (
+  < >
+  <div className="pane top-panel">
+    <Editor 
+    language="xml" 
+    displayName="HTML" 
+    value={html} 
+    onChange={setHTML} 
+    //hook set  to change the state everytime we pass a text
+    />
+    <Editor 
+    language="css" 
+    displayName="CSS" 
+    value={css} 
+    onChange={setCSS} 
+    />
+    <Editor 
+    language="javascript" 
+    displayName="JavaScript" 
+    value={js} 
+    onChange={setJS} 
+    />
+  </div>
+  <div className = "pane">
+    <iframe 
+      title ="output"
+      sandbox = "allow-scripts"
+      frameBorder="0"
+      width ="100%"
+      height="100%"
+
+      />
+  </div>
+  </>
+  )
 }
 
 export default App;
